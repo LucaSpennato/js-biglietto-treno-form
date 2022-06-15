481aed008a140f21ea9aba7e7746c9886a163360
LucaSpennato <luca.spennato@gmail.com>
2022-06-15 23:39:23 +0200

    mileston 2 w/ ugly-layout

--- js/script.js
+++ js/script.js
@@ -39,82 +39,98 @@ submitBtn.addEventListener('click', function(){
 
     let userName = document.getElementById('name_input').value;
     console.log (userName);
-    document.getElementById('user_name_info').innerHTML += `<div>${userName}</div>`
+    document.getElementById('user_name_info').innerHTML += `<div>${userName}</div>`;
 
     let ageSelector = document.getElementById('age_choose').value;
     // ageSelector = parseInt(ageSelector);
     // per altro metodo, dare le variabili di sconto direttamente nel value e fare il parseint, successivamente vanno messe nella condizione sotto
     console.log (ageSelector);
 
+    let cabNumberGenerator = (Math.floor(Math.random() * 10) + 1 );
+    document.getElementById('cab_number').innerHTML += `<div>${cabNumberGenerator}</div>`;
+
+    let ticketNumberGenerator = (Math.floor(Math.random() * 999999) + 1 );
+    document.getElementById('ticket_number').innerHTML += `<div>${ticketNumberGenerator}</div>`;
     // prezzo del biglietto!
     let ticketPrice = travelDistanceKm * pricePerKm;
+    ticketPrice = ticketPrice.toFixed(2);
     console.log (ticketPrice);
 
     // facciam vedere in pagina i km da percorrere (chiedere domani, perchè non si vedono i decimali?)
-    document.querySelector('main .info_display_wrapper .user_taught_km').append(` ${travelDistanceKm} Km!`)
+    document.querySelector('main .info_display_wrapper .user_taught_km').append(` ${travelDistanceKm} Km!`);
 
-    if (ageSelector == 'minor'){
-        ageSelector = 15
-        discountApply = ((ticketPrice * ageSelector) / 100);
-        discountedTicketPrice = (ticketPrice - discountApply);
-        discountedTicketPrice = discountedTicketPrice.toFixed(2)
-        console.log ('Prezzo del biglietto con sconto under 18: ' + ticketPrice);
+    // con if isnan(travelDistanceKm) verifichiamo che l'utente abbia inserito dei numeri, altrimenti parte un alert
+    if (isNaN(travelDistanceKm)){
+        alert('Non hai inserito un numero nella selezione kilometri, ricarica la pagina');
+        location.reload();
+    } else{
+        if (ageSelector == 'minor'){
+            ageSelector = 15;
+            discountApply = ((ticketPrice * ageSelector) / 100);
+            discountedTicketPrice = (ticketPrice - discountApply);
 
-        // facciam vedere i dati con la scontistica applicata:
-        // in questo caso rimuoviamo d-none, tanto per provarle tutte (e credo sia anche più corretto dato che d-none resta se inseriamo d-block)
-        // NEL CASO IN CUI VOLESSIMO AGGIUNGERE CLASSI:
-        // document.querySelector('main .user_pricing_infos div').classList.add ('text-danger', 'text-center', 'active'); E CI PERMETTE DI METTERNE PIù DI UNA
-        document.querySelector('main .user_pricing_infos div').classList.remove ('d-none');
+            discountApply = discountApply.toFixed(2);
+            discountedTicketPrice = discountedTicketPrice.toFixed(2);
 
-        // inseriamo i valori nei div appositi
-        document.querySelector('main .user_pricing_infos>div>div:first-child').append(` ${ticketPrice} €`);
-        document.querySelector('main .user_pricing_infos>div>div:nth-child(2)').append(` 'Under 18'`)
-        document.querySelector('main .user_pricing_infos>div>div:nth-child(3)').append(` ${discountApply} €`);
-
-        // prezzo finale:
-        document.getElementById('user_ticket_price').append(` ${discountedTicketPrice} €`);
-
-    } else if (ageSelector == 'elder') {
-        ageSelector = 35;
-        discountApply = ((ticketPrice * ageSelector) / 100);
-        discountedTicketPrice = (ticketPrice - discountApply);
-        discountedTicketPrice = discountedTicketPrice.toFixed(2)
-
-        console.log('Prezzo del bbiglietto con sconto over 65: ' + ticketPrice);
-        
-        // facciam vedere i dati di scontistica applicata:
-        document.querySelector('main .user_pricing_infos div').classList.remove ('d-none');
-
-         // inseriamo i valori nei div appositi
-         document.querySelector('main .user_pricing_infos>div>div:first-child').append(` ${ticketPrice} €`);
-         document.querySelector('main .user_pricing_infos>div>div:nth-child(2)').append(` 'Over 65'`)
-         document.querySelector('main .user_pricing_infos>div>div:nth-child(3)').append(` ${discountApply} €`);
-
-          // prezzo finale:
-        document.getElementById('user_ticket_price').append(` ${discountedTicketPrice} €`);
-
-    } else {
-        ticketPrice = ticketPrice.toFixed(2)
-        console.log('Prezzo del biglietto: ' + ticketPrice);
-        
-
-        // facciamo in modo che appaia un messaggio di scontistica non applicata nel caso in cui non rientri nei parametri
-
-        // 1 METODO, GLI DIAMO D-NONE IN HTML E POI D-BLOCK NELLE CLASSI
-        // let noDiscount = document.getElementById('no_discount');
-        // noDiscount.classList = 'd-block';
-        // OPPURE: document.querySelector('main .user_pricing_infos #no_discount').className += 'd-block';
-
-        //NOTE BENE: usando però un classList('') o className(''),
-        // OPPURE class.List('') o className ('') += 'qualcosa' STAI AGGIUNGENDO
-        //   AGGIUNGI SOLO CLASSI, 
-        // ne vuoi rimuovere? classList o ClassName .remove('')
-
-        // ALTRO DI INFINITI MEDOTI, MA IN QUESTO CREIAMO IL DIV
-        document.querySelector('main .user_pricing_infos').innerHTML += '<div>Nessuna Scontistica è stata applicata.</div>'
-
-        // prezzo finale:
-        document.getElementById('user_ticket_price').append(` ${ticketPrice} €`);
+            console.log ('Prezzo del biglietto con sconto under 18: ' + discountedTicketPrice);
+    
+            // facciam vedere i dati con la scontistica applicata:
+            // in questo caso rimuoviamo d-none, tanto per provarle tutte (e credo sia anche più corretto dato che d-none resta se inseriamo d-block)
+            // NEL CASO IN CUI VOLESSIMO AGGIUNGERE CLASSI:
+            // document.querySelector('main .user_pricing_infos div').classList.add ('text-danger', 'text-center', 'active'); E CI PERMETTE DI METTERNE PIù DI UNA
+            document.querySelector('main .user_pricing_infos div').classList.remove ('d-none');
+    
+            // inseriamo i valori nei div appositi
+            document.querySelector('main .user_pricing_infos>div>div:first-child').append(` ${ticketPrice} €`);
+            document.querySelector('main .user_pricing_infos>div>div:nth-child(2)').append(` 'Under 18'`);
+            document.querySelector('main .user_pricing_infos>div>div:nth-child(3)').append(` ${discountApply} €`);
+    
+            // prezzo finale:
+            document.getElementById('user_ticket_price').append(` ${discountedTicketPrice} €`);
+    
+        } else if (ageSelector == 'elder') {
+            ageSelector = 35;
+            discountApply = ((ticketPrice * ageSelector) / 100);
+            discountedTicketPrice = (ticketPrice - discountApply);
+            
+            discountApply = discountApply.toFixed(2);
+            discountedTicketPrice = discountedTicketPrice.toFixed(2);
+    
+            console.log('Prezzo del bbiglietto con sconto over 65: ' + ticketPrice);
+            
+            // facciam vedere i dati di scontistica applicata:
+            document.querySelector('main .user_pricing_infos div').classList.remove ('d-none');
+    
+             // inseriamo i valori nei div appositi
+             document.querySelector('main .user_pricing_infos>div>div:first-child').append(` ${ticketPrice} €`);
+             document.querySelector('main .user_pricing_infos>div>div:nth-child(2)').append(` 'Over 65'`);
+             document.querySelector('main .user_pricing_infos>div>div:nth-child(3)').append(` ${discountApply} €`);
+    
+              // prezzo finale:
+            document.getElementById('user_ticket_price').append(` ${discountedTicketPrice} €`);
+    
+        } else {
+            console.log('Prezzo del biglietto: ' + ticketPrice);
+            
+    
+            // facciamo in modo che appaia un messaggio di scontistica non applicata nel caso in cui non rientri nei parametri
+    
+            // 1 METODO, GLI DIAMO D-NONE IN HTML E POI D-BLOCK NELLE CLASSI
+            // let noDiscount = document.getElementById('no_discount');
+            // noDiscount.classList = 'd-block';
+            // OPPURE: document.querySelector('main .user_pricing_infos #no_discount').className += 'd-block';
+    
+            //NOTE BENE: usando però un classList('') o className(''),
+            // OPPURE class.List('') o className ('') += 'qualcosa' STAI AGGIUNGENDO
+            //   AGGIUNGI SOLO CLASSI, 
+            // ne vuoi rimuovere? classList o ClassName .remove('')
+    
+            // ALTRO DI INFINITI MEDOTI, MA IN QUESTO CREIAMO IL DIV
+            document.querySelector('main .user_pricing_infos').innerHTML += '<div>Nessuna Scontistica è stata applicata.</div>'
+    
+            // prezzo finale:
+            document.getElementById('user_ticket_price').append(` ${ticketPrice} €`);
+        }
     }
 
 })
